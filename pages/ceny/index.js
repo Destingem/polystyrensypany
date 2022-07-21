6a9fe18fce5598e7935224439869f94931282066
Ondřej Zaplatílek <ondrej.zaplatilek@gmail.com>
2022-07-21 21:03:07 +0200

    Ceny

---
+++ pages/ceny/index.js
@@ -0,0 +1,16 @@
+import Media from  '../../Components/Media';
+export default function Ceny() {
+  return (
+   <>
+    <Media between=["zero", "mobile"]>
+        <CenyMobile />
+    </Media>
+    <Media between=["mobile", "tablet"]>
+        <CenyTablet />
+    </Media>
+    <Media between=["tablet", "laptop"]>
+        <CenyLaptop />  
+    </Media>
+   </>
+  );
+}
