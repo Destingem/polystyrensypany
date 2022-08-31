bc3a73b1d3470011f658f62172995c313f391cc7
Ondřej Zaplatílek <ondrej.zaplatilek@gmail.com>
2022-08-31 12:16:25 +0200

    ceny edit

--- pages/ceny/index.js
+++ pages/ceny/index.js
@@ -2,13 +2,13 @@ import Media from  '../../Components/Media';
 export default function Ceny() {
   return (
    <>
-    <Media between=["zero", "mobile"]>
+    <Media between={["zero", "mobile"]}>
         <CenyMobile />
     </Media>
-    <Media between=["mobile", "tablet"]>
+    <Media between={["mobile", "tablet"]}>
         <CenyTablet />
     </Media>
-    <Media between=["tablet", "laptop"]>
+    <Media between={["tablet", "laptop"]}>
         <CenyLaptop />  
     </Media>
    </>
