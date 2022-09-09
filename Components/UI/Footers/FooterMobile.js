5712baf3fe27a563f6da24ba4b269d81da138483
Ondřej Zaplatílek <ondrej.zaplatilek@gmail.com>
2022-09-09 18:54:19 +0200

    mobile footer

--- Components/UI/Footers/FooterMobile.js
+++ Components/UI/Footers/FooterMobile.js
@@ -1,7 +1,82 @@
+import {Text, Grid} from "@mantine/core";
 export default function FooterMobile() {
-    return(
-        <div style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#327b62", height: "8vh"}} >
-        <p style={{color: "#c4c4c4"}}>Mobile</p>
+  return (
+    <div
+      style={{
+    display: "flex",
+    flexDirection: "column",
+       alignItems: "center",
+        backgroundColor: "white",
+        maxHeight: "80vh",
+        maxWidth: "100%",
+        overflow: "hidden",
+
+      }}
+    >
+        
+        <div style={{ backgroundColor: "#e5e5e5", minHeight: "50vh",minWidth: "100%",display: "flex",flexDirection: "column",gap: "0",paddingTop: "8vh",alignItems: "center",textAlign: "center",paddingLeft: "4vw",paddingRight: "4vw",zIndex: "25"}}>
+        <div style={{marginTop: "10vh",   backgroundColor:" #3f7b63",minWidth: "150vw",minHeight: "150vw",borderRadius: "50%", marginTop: "-130vw",zIndex: "2500", alignItems: "center", justifyContent: "center", marginLeft: "-30vw" }}>
+        <Text component="h3"  sx={{ color: "white", fontSize: "8vw", margin: "0", marginTop: "118vw", marginLeft: "5vw" }}>Polystyren sypaný</Text>
+        <Text color="#3f7b63" component="em" sx={{ color: "white", fontSize: "5vw", margin: "0" }}>ta pravá izolace</Text>
+        </div>
+         <div style={{display: "flex", flexDirection: "row", maxWidth: "100vw", padding: "0 5vw 3vh 5vw" }}>
+        <div>
+        <Text
+            component="h4"
+            sx={{
+              color: "#3f7b63",
+              fontSize: "8vw",
+              marginTop: "2vh",
+              marginBottom: "1vh",
+            }}
+          >
+            Kontakt
+          </Text>
+          <Grid>
+            <Grid.Col>
+              <Text
+                component="p"
+                sx={{ color: "#3f7b63", fontSize: "4vw", margin: "0" }}
+              >
+                Telefon 1: +420 723 513 638
+              </Text>
+              <Text
+                component="p"
+                sx={{ color: "#3f7b63", fontSize: "4vw", margin: "0" }}
+              >
+                Telefon 2: +420 737 226 697
+              </Text>
+              <Text
+                component="p"
+                sx={{ color: "#3f7b63", fontSize: "4vw", margin: "0" }}
+              >
+                Mail: info@polystyrensypany.cz
+              </Text>
+            </Grid.Col>
+          </Grid>
+        </div>
+        <div>
+        <Text
+            component="h4"
+            sx={{
+              color: "#3f7b63",
+              fontSize: "8vw",
+              marginTop: "2vh",
+              marginBottom: "1vh",
+            }}
+          >
+            Výroba a prodej
+          </Text>
+          <Text
+            component="p"
+            sx={{ color: "#3f7b63", fontSize: "4vw", margin: "0" }}
+          >
+            Polystyren sypaný - Richard Humeš
+            <br /> Štěpánovská 330 190 17 Praha 9 - Vinoř
+          </Text>
+        </div>
+         </div>
+         </div>
     </div>
-    )
+  );
 }
--- Components/UI/Footers/FooterTablet.js
+++ Components/UI/Footers/FooterTablet.js
@@ -1,7 +1,8 @@
+import FooterLaptop from "./FooterLaptop";
+import FooterMobile from "./FooterMobile";
+
 export default function FooterTablet() {
     return(
-        <div style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#327b62", height: "8vh"}} >
-        <p style={{color: "#c4c4c4"}}>Tablet</p>
-    </div>
+       <FooterLaptop />
     )
 }
