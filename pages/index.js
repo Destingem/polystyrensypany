804f843f9f3693bdbcdc4c772113ab3434e4f08b
Ondřej Zaplatílek <ondrej.zaplatilek@gmail.com>
2022-10-12 13:03:44 +0200

    analytics

--- pages/index.js
+++ pages/index.js
@@ -10,28 +10,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home(props) {
  
-  useEffect(() => {
-    var _gaq = _gaq || [];
-
-    _gaq.push(["_setAccount", "UA-27461326-2"]);
-
-    _gaq.push(["_trackPageview"]);
-
-    (function () {
-      var ga = document.createElement("script");
-      ga.type = "text/javascript";
-      ga.async = true;
-
-      ga.src =
-        ("https:" == document.location.protocol
-          ? "https://ssl"
-          : "http://www") + ".google-analytics.com/ga.js";
-
-      var s = document.getElementsByTagName("script")[0];
-      s.parentNode.insertBefore(ga, s);
-    })();
-
-  }, []);
+  
   return (
     <div className={styles.container}>
       <Head>
