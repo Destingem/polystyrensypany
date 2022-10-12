dbda48e0b73a165c8b90be8a5bac69dd6c23be69
Ondřej Zaplatílek <ondrej.zaplatilek@gmail.com>
2022-10-12 13:19:18 +0200

    google analytics #3

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import {MantineProvider} from "@mantine/core"
 import { MediaContextProvider } from "../Components/Media"
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 function MyApp({ Component, pageProps }) {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
   const [showChild, setShowChild] = useState(false);
@@ -10,28 +11,7 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     setShowChild(true);
   }, []);
-  useEffect(() => {
-    var _gaq = _gaq || [];
-
-    _gaq.push(["_setAccount", "UA-27461326-2"]);
-
-    _gaq.push(["_trackPageview"]);
-
-    (function () {
-      var ga = document.createElement("script");
-      ga.type = "text/javascript";
-      ga.async = true;
-
-      ga.src =
-        ("https:" == document.location.protocol
-          ? "https://ssl"
-          : "http://www") + ".google-analytics.com/ga.js";
-
-      var s = document.getElementsByTagName("script")[0];
-      s.parentNode.insertBefore(ga, s);
-    })();
-
-  }, []);
+  
 
   if (!showChild) {
     return null;
@@ -51,20 +31,9 @@ function MyApp({ Component, pageProps }) {
   >
   <Head>
 
-  <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-27461326-2', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
 
   </Head>
+  <GoogleAnalytics trackPageViews gaMeasurementId="UA-27461326-2"/>
   <Component {...pageProps} />
   </MantineProvider>
   </MediaContextProvider>
